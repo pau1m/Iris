3d6fc4186b42ded3776c3928de25cd895219280e
pau1m <paul@citywebconsultants.co.uk>
2016-02-23 16:33:03 +0000

    Testing test

--- iris_core/test/user_crud_spec.js
+++ iris_core/test/user_crud_spec.js
@@ -22,56 +22,63 @@ var testUser = {
 testUser.login.username = generateString(5);
 testUser.login.password = generateString(10);
 
-frisby.create('Request auth key')
-  .post(baseURL + '/api/login',
-  adminUser.login,
-  {
-      json: true
-  })
-  .expectStatus(200)
-  .expectHeaderContains('content-type', 'application/json')
-  .expectJSONTypes({
-      userid: String,
-      token: String
-  })
-  .afterJSON(function (res) {
-      adminUser.auth.token = res.token;
+frisby.create('Set first time user')
+  .post(baseURL + '/api/user/first',
+   adminUser.login,
+   { json: true })
+  .after(function (res) {
+  frisby.create('Request auth key')
+    .post(baseURL + '/api/login',
+    adminUser.login,
+    {
+        json: true
+    })
+    .expectStatus(200)
+    .expectHeaderContains('content-type', 'application/json')
+    .expectJSONTypes({
+        userid: String,
+        token: String
+    })
+    .afterJSON(function (res) {
+        adminUser.auth.token = res.token;
 
-      frisby.create('Create a user')
-          .post(baseURL + '/entity/create/user',
-            {
-                credentials: adminUser.auth,
-                username: testUser.login.username,
-                password: testUser.login.password,
-                roles:  testUser.auth.roles
-            },
-            { json: true })
-          .expectStatus(200)
-          .expectHeaderContains('content-type', 'application/json')
-          .expectJSONTypes({
-              eid: Number,
-              roles: Array,
-              username: String,
-              password: String,
-              entityType: String
-          })
-          .inspectJSON()
-          .inspectBody()
-          .afterJSON(function (res) {
-                testUser.auth.userid = res.eid;
-                frisby.create('Update user roles with admin')
-                    .post(baseURL + '/entity/edit/user/' + testUser.auth.userid,
-                    {
-                        credentials: adminUser.auth,
-                        roles: ['admin']
-                    },
-                    { json : true })
-                    .expectStatus(200)
-                    .inspectJSON()
-                    .expectHeaderContains('content-type', 'application/json')
-                    .toss()        
+        frisby.create('Create a user')
+            .post(baseURL + '/entity/create/user',
+              {
+                  credentials: adminUser.auth,
+                  username: testUser.login.username,
+                  password: testUser.login.password,
+                  roles:  testUser.auth.roles
+              },
+              { json: true })
+            .expectStatus(200)
+            .expectHeaderContains('content-type', 'application/json')
+            .expectJSONTypes({
+                eid: Number,
+                roles: Array,
+                username: String,
+                password: String,
+                entityType: String
             })
-            .toss()
-  })
-  .inspectJSON()
-  .toss();
+            .inspectJSON()
+            .inspectBody()
+            .afterJSON(function (res) {
+                  testUser.auth.userid = res.eid;
+                  frisby.create('Update user roles with admin')
+                      .post(baseURL + '/entity/edit/user/' + testUser.auth.userid,
+                      {
+                          credentials: adminUser.auth,
+                          roles: ['admin']
+                      },
+                      { json : true })
+                      .expectStatus(200)
+                      .inspectJSON()
+                      .expectHeaderContains('content-type', 'application/json')
+                      .toss()        
+              })
+              .toss()
+    })
+    .inspectJSON()
+    .toss()
+})
+.toss();
